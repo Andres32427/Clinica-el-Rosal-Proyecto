b003e21fe6c13c7bf826e11aa10a1247448805f2
Andrés32427 <pipealfa11@hotmail.com>
2024-12-16 18:46:23 -0500

    Creacion de la Lista de persona entity

---
+++ Back- Clinica el Rosal/Clinica el Rosal/Clinica-el-Rosal/src/main/java/co/edu/sena/Clinica/el/Rosal/Entity/PersonaEntity.java
@@ -0,0 +1,64 @@
+package co.edu.sena.Clinica.el.Rosal.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Data
+@Entity
+@Table (name = "Persona")
+public class PersonaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "genero")
+    private String genero;
+
+    @Column(name = "fecha_nacimiento")
+    private Date fechaNacimiento;
+
+    @Column(name = "tipo_identificacion")
+    private String tipoIdentificacion;
+
+    @Column(name = "identificacion")
+    private String identificacion;
+
+    @Column(name = "id_seguro")
+    private String idSeguro;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "correo")
+    private String correo;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "grupo_sangineo")
+    private String grupoSanguineo;
+
+    @Column(name = "alergias")
+    private String alergias;
+
+    @Column(name = "Tipo_de_Alergia")
+    private String tipoDeAlergia;
+
+    @Column(name = "id_municipio")
+    private Long idMunicipio;
+}
