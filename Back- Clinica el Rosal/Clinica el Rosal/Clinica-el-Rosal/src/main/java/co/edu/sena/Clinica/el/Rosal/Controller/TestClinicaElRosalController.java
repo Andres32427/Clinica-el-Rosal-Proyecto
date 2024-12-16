0e62fd8e447cc92618008210512ded44638dd28b
Andrés32427 <pipealfa11@hotmail.com>
2024-12-16 13:12:00 -0500

    Se implementa Controlador web para Pruebas

--- Back- Clinica el Rosal/Clinica el Rosal/Clinica-el-Rosal/src/main/java/co/edu/sena/Clinica/el/Rosal/Controller/TestClinicaElRosalController.java
+++ Back- Clinica el Rosal/Clinica el Rosal/Clinica-el-Rosal/src/main/java/co/edu/sena/Clinica/el/Rosal/Controller/TestClinicaElRosalController.java
@@ -1,4 +1,17 @@
 package co.edu.sena.Clinica.el.Rosal.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/TestClinicaElRosal")
 public class TestClinicaElRosalController {
+
+
+
+    @GetMapping()
+    public String BienvenidoalaclinicaelRosal(){
+        return "Bienvenido a la clinica el Rosal";
+    }
 }
