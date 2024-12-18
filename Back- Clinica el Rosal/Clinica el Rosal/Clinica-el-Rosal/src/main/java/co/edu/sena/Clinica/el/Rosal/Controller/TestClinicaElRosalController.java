708ed0d83ac977b3bbb992acac25a9e67d942cba
Andrés32427 <pipealfa11@hotmail.com>
2024-12-18 15:18:21 -0500

    Add Packages

--- Back- Clinica el Rosal/Clinica el Rosal/Clinica-el-Rosal/src/main/java/co/edu/sena/Clinica/el/Rosal/Controller/TestClinicaElRosalController.java
+++ Back- Clinica el Rosal/Clinica el Rosal/Clinica-el-Rosal/src/main/java/co/edu/sena/Clinica/el/Rosal/Controller/TestClinicaElRosalController.java
@@ -1,17 +1,46 @@
 package co.edu.sena.Clinica.el.Rosal.Controller;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.sena.Clinica.el.Rosal.Service.PersonaService;
+import co.edu.sena.Clinica.el.Rosal.dto.PersonaDto;
 
 @RestController
 @RequestMapping("/TestClinicaElRosal")
 public class TestClinicaElRosalController {
 
+    @Autowired
+    private PersonaService service;
+
+
 
 
     @GetMapping()
     public String BienvenidoalaclinicaelRosal(){
+        
+        PersonaDto dto = new PersonaDto();
+        dto.setNombre("Wilches");
+        dto.setApellido("Ochoa");
+        dto.setGenero("M");
+        dto.setFechaNacimiento(new Date());
+        dto.setTipoIdentificacion("Registro Civil");
+        dto.setIdentificacion("109847047");
+        dto.setIdSeguro("10");
+        dto.setTelefono("320614910");
+        dto.setCorreo("Federico34@gmail.com");
+        dto.setDireccion("Calle40#7e-25");
+        dto.setGrupoSanguineo("AB-");
+        dto.setAlergias("No");
+        dto.setTipoDeAlergia("N/A");
+        dto.setIdMunicipio(1095L);
+
+        service.save(dto);
+        
         return "Bienvenido a la clinica el Rosal";
     }
 
--- Back- Clinica el Rosal/Clinica el Rosal/Clinica-el-Rosal/src/main/java/co/edu/sena/Clinica/el/Rosal/Service/PersonaService.java
+++ Back- Clinica el Rosal/Clinica el Rosal/Clinica-el-Rosal/src/main/java/co/edu/sena/Clinica/el/Rosal/Service/PersonaService.java
@@ -3,6 +3,8 @@ package co.edu.sena.Clinica.el.Rosal.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import co.edu.sena.Clinica.el.Rosal.dto.PersonaDto;
+import co.edu.sena.Clinica.el.Rosal.Entity.PersonaEntity;
 import co.edu.sena.Clinica.el.Rosal.Repository.PersonaRepository;
 
 @Service
@@ -11,4 +13,24 @@ public class PersonaService {
     @Autowired
     private PersonaRepository Repository;
 
+    public void save (PersonaDto dto){
+
+        PersonaEntity entity = new PersonaEntity();
+        entity.setNombre(dto.getNombre());
+        entity.setApellido(dto.getApellido());
+        entity.setGenero(dto.getGenero());
+        entity.setFechaNacimiento(dto.getFechaNacimiento());
+        entity.setTipoIdentificacion(dto.getTipoIdentificacion());
+        entity.setIdentificacion(dto.getIdentificacion());
+        entity.setIdSeguro(dto.getIdSeguro());
+        entity.setTelefono(dto.getTelefono());
+        entity.setCorreo(dto.getCorreo());
+        entity.setDireccion(dto.getDireccion());
+        entity.setGrupoSanguineo(dto.getGrupoSanguineo());
+        entity.setAlergias(dto.getAlergias());
+        entity.setTipoDeAlergia(dto.getTipoDeAlergia());
+        entity.setIdMunicipio(dto.getIdMunicipio());
+
+        Repository.save(entity);
+    }
 }
